cfe49ac603e3a07f407bae56893728673746317c
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-12-01 13:06:24 +0100

    Use LoadHTMLGlob() instead of parsing templates manually. Use maps instead of a anonymous struct.

--- gin/benchmarker/server.go
+++ gin/benchmarker/server.go
@@ -2,13 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"html/template"
 )
-
-type People []Person
-type Person struct {
-	Name string
-}
 
 func main() {
 	// Creates a router without any middleware by default
@@ -18,28 +12,18 @@ func main() {
 	r.Use(gin.Recovery())
 
 	// Setup templates
-	html := template.Must(template.ParseFiles("./templates/layout.tmpl", "./templates/index.tmpl", "./templates/bio.tmpl"))
-	r.SetHTMLTemplate(html)
+	r.LoadHTMLGlob("templates/*")
 
 	// Action handler
 	r.GET("/:title", func(c *gin.Context) {
 		title := c.Params.ByName("title")
-		members := People{
-			Person{Name: "Chris McCord"},
-			Person{Name: "Matt Sears"},
-			Person{Name: "David Stump"},
-			Person{Name: "Ricardo Thompson"},
-		}
+		members := []gin.H{
+			{"Name": "Chris McCord"},
+			{"Name": "Matt Sears"},
+			{"Name": "David Stump"},
+			{"Name": "Ricardo Thompson"}}
 
-		context := struct {
-			Title   string
-			Members People
-		}{
-			title,
-			members,
-		}
-
-		c.HTML(200, "layout.tmpl", context)
+		c.HTML(200, "layout.tmpl", gin.H{"Title": title, "Members": members})
 	})
 
 	// Run the server
