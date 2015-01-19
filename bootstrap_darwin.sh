5f53b79c6aae14d32c6d0806d42fb1057d2f6545
Matthew Rothenberg <mrothenberg@gmail.com>
2015-01-19 11:51:59 -0500

    bootstrapping for ruby platforms

---
+++ bootstrap_darwin.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to bootstrap all dependencies.
+# Currently only works on a Darwin (MacOSX system), and utilizes homebrew.
+# TODO / HELP WANTED: update this to work on other *nixes (and change filename).
+
+# Install Homebrew if not there already.
+if [ ! -f  /usr/local/bin/brew ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+# bootstrap rails/sinatra
+brew install rbenv
+rbenv install --skip-existing 2.2.0
+(cd rails/benchmarker   && bundle install)
+(cd sinatra/benchmarker && bundle install)
+
+# TODO: bootstrap express
+
+# TODO: bootstrap martini
+
+# TODO: bootstrap phoenix
+
+# TODO: bootstrap gin
+
+# TODO: bootstrap play
