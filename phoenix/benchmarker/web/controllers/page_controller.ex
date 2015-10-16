912cf8e9f5be1d2fd6908034fdc10d37b78e13dc
Emilio Larrambebere <emiliolarrambebere@gmail.com>
2015-10-16 22:18:23 +0100

    Upgrade Phoenix to version 1.0

--- phoenix/benchmarker/web/controllers/page_controller.ex
+++ phoenix/benchmarker/web/controllers/page_controller.ex
@@ -1,8 +1,6 @@
 defmodule Benchmarker.PageController do
   use Benchmarker.Web, :controller
 
-  plug :action
-
   def index(conn, %{"title" => title}) do
     render conn, "index.html", title: title, members: [
       %{name: "Chris McCord"},
